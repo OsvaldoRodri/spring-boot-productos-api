a74815b79808b4dc891871f18584c242274f8265
Osvaldo Rodríguez <133803822+OsvaldoRodri@users.noreply.github.com>
2026-06-14 23:41:45 -0600

    feat: add Spring Boot main application entry point

---
+++ src/main/java/dev/osvaldo/productos/ProductosApplication.java
@@ -0,0 +1,12 @@
+package dev.osvaldo.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductosApplication.class, args);
+    }
+}
